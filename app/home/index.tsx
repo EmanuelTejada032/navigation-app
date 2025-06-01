014d17500f669765477ff34fdc2734c3bc80818c
EmanuelTejada032 <emanueltejada31295@gmail.com>
2025-06-01 17:25:33 -0400

    add custom button with forward ref, extend pressableprops

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,16 +1,26 @@
-import { Link } from "expo-router";
+import CustomButton from "@/components/CustomButton";
+import { Link, router } from "expo-router";
 import React from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+
+
   return (
     <SafeAreaView>
-      <View>
+      <View className="px-10">
         <Text className="text-4xl mb-4 text-primary mt-5">HomeScreen</Text>
-        <Link className='text-2xl mb-5 text-secondary-200 font-mont-light' href={"/products"}> Products</Link>
+        <Link href='/products' asChild>
+          <CustomButton color="primary" className="mb-4"> Products</CustomButton>
+        </Link>
+
+        <CustomButton color="primary" className="mb-4" onPress={() => router.push('/products')}> Products</CustomButton>
+        <CustomButton variant="text-only" className="mb-4" color="tertiary" onPress={() => router.push('/products')}> Products</CustomButton>
+
+        {/* <Link className='text-2xl mb-5 text-secondary-200 font-mont-light' href={"/products"}> Products</Link>
         <Link className='text-2xl mb-5 text-secondary-200 font-mont-light' href={"/profile"}> Profile</Link>
-        <Link className='text-2xl mb-5 text-secondary-200 font-mont-light' href={"/settings"}> Settings</Link>
+        <Link className='text-2xl mb-5 text-secondary-200 font-mont-light' href={"/settings"}> Settings</Link> */}
       </View>
     </SafeAreaView>
   );
