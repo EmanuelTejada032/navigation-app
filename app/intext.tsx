f227adc232b054cf3f7bf64ff59c0c259dcaed11
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2025-06-01 14:37:31 -0400

    project first commit

---
+++ app/intext.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const intext = () => {
+  return (
+    <View>
+      <Text>Hello world text</Text>
+    </View>
+  )
+}
+
+export default intext
