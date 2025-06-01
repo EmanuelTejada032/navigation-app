6b31b48a6c2861daa8d1b1dae18d9ab2ea83eaae
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2025-06-01 15:24:23 -0400

    nativewind setup

--- app/intext.tsx
+++ app/intext.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Text, View } from 'react-native'
 
 const intext = () => {
   return (
-    <View>
-      <Text>Hello world text</Text>
+    <View className='mt-10'>
+      <Text className='text-3xl font-bold text-blue-500'>Hello world text</Text>
     </View>
   )
 }
