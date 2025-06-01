0c8ce09615099d8f23ada8ec43e9eb6c270b2442
EmanuelTejada032 <emanueltejada31295@gmail.com>
2025-06-01 15:55:47 -0400

    Importing fonts

--- app/intext.tsx
+++ app/intext.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const intext = () => {
   return (
-    <View className='mt-10'>
-      <Text className='text-3xl font-bold text-blue-500'>Hello world text</Text>
-    </View>
+    <SafeAreaView>
+      <View className='mt-6 mx-3'>
+        <Text className='text-3xl font-mont-bold'>Hello world text</Text>
+        <Text className='text-4xl font-mont-black'>Hello world text</Text>
+        <Text className='text-2xl font-mont-light'>Hello world text</Text>
+        <Text className='text-xl font-mont-medium'>Hello world text</Text>
+        <Text className='text-2xl'>Hello world text</Text>
+
+      </View>
+    </SafeAreaView>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'mont-black': ['Montserrat-Black', 'sans-serif'],
+        'mont-bold': ['Montserrat-Bold', 'sans-serif'],
+        'mont-light': ['Montserrat-Light', 'sans-serif'],
+        'mont-medium': ['Montserrat-Medium', 'sans-serif'],
+        'mont-regular': ['Montserrat-Regular', 'sans-serif'],
+        'mont-semibold': ['Montserrat-SemiBold', 'sans-serif'],
+        'mont-thin': ['Montserrat-Thin', 'sans-serif'],
+        
+      }
+    },
   },
   plugins: [],
 }
